3c6a262e282ee73445ec51d3623e504b9cb6a2fe
Shakespearen <alokin123321@gmail.com>
2024-02-01 22:20:35 +0100

    End of section 11

--- DatingApp/Controllers/AccountController.cs
+++ DatingApp/Controllers/AccountController.cs
@@ -39,13 +39,16 @@ namespace DatingApp.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username);
+            var user = await _context.Users
+                .Include(p => p.Photos)
+                .FirstOrDefaultAsync(x => x.UserName == loginDto.Username);
 
             if (user == null) return Unauthorized("Invalid  username");
 
@@ -61,7 +64,8 @@ namespace DatingApp.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
---
+++ DatingApp/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Claims;
+
+namespace DatingApp.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUsername(this ClaimsPrincipal user) { 
+            return user.FindFirst(ClaimTypes.NameIdentifier).Value; 
+        }
+    }
+}
